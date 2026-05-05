39298ecb292c47fd7da1881a0fdbc923348e69cc
Jakkoble <hey@jakob.foo>
2026-05-05 17:20:29 +0200

    format code to match convention

--- src/Commander/Commander.Infrastructure/Adapters/DockerRunnerAdapter.cs
+++ src/Commander/Commander.Infrastructure/Adapters/DockerRunnerAdapter.cs
@@ -8,65 +8,65 @@ namespace Commander.Infrastructure.Adapters;
 
 public class DockerRunnerOptions
 {
-  public string Image { get; set; } = "hexatask-runner:latest";
+    public string Image { get; set; } = "hexatask-runner:latest";
 }
 
-public class DockerRunnerAdapter(IDockerClient client, IOptions<DockerRunnerOptions> opts) : IRunnerPort
+public class DockerRunnerAdapter(IDockerClient client, IOptions<DockerRunnerOptions> opts)
+    : IRunnerPort
 {
-  private readonly IDockerClient _client = client;
-  private readonly string _image = opts.Value.Image;
-  private readonly bool _isImageRemote = opts.Value.Image.Contains('/');
+    private readonly IDockerClient _client = client;
+    private readonly string _image = opts.Value.Image;
+    private readonly bool _isImageRemote = opts.Value.Image.Contains('/');
 
-  public async Task ExecuteJob(Job job)
-  {
-    await EnsureImageAsync();
-
-    var response = await _client.Containers.CreateContainerAsync(new CreateContainerParameters()
+    public async Task ExecuteJob(Job job)
     {
-      Image = _image,
-      Name = ContainerName(job),
-      Env = [
-        $"JOB_ID={job.Id}",
-        $"COMMANDER_URL=host.docker.internal:5271"
-      ],
-      HostConfig = new()
-      {
-        AutoRemove = true,
-	ExtraHosts = ["host.docker.internal:host-gateway"],
-      }
-    });
+        await EnsureImageAsync();
 
-    await _client.Containers.StartContainerAsync(response.ID, new());
-  }
-
-  private static string ContainerName(Job job) => $"hexatask-{job.Id}";
-
-  public async Task EnsureImageAsync()
-  {
-    if (_isImageRemote)
-    {
-      await _client.Images.CreateImageAsync(
-            new ImagesCreateParameters { FromImage = _image },
-            authConfig: null,
-            new Progress<JSONMessage>()
+        var response = await _client.Containers.CreateContainerAsync(
+            new CreateContainerParameters()
+            {
+                Image = _image,
+                Name = ContainerName(job),
+                Env = [$"JOB_ID={job.Id}", $"COMMANDER_URL=host.docker.internal:5271"],
+                HostConfig = new()
+                {
+                    AutoRemove = true,
+                    ExtraHosts = ["host.docker.internal:host-gateway"],
+                },
+            }
         );
 
-      return;
+        await _client.Containers.StartContainerAsync(response.ID, new());
     }
 
-    var images = await _client.Images.ListImagesAsync(
-        new ImagesListParameters
-        {
-          Filters = new Dictionary<string, IDictionary<string, bool>>()
-          {
-            ["reference"] = new Dictionary<string, bool> { [_image] = true }
-          }
-        }
-    );
+    private static string ContainerName(Job job) => $"hexatask-{job.Id}";
 
-    if (images.Count == 0)
-      throw new InvalidOperationException(
-          $"Local runner image '{_image}' not found. Run at project root: docker build -t {_image} ./src/Runner"
-      );
-  }
+    public async Task EnsureImageAsync()
+    {
+        if (_isImageRemote)
+        {
+            await _client.Images.CreateImageAsync(
+                new ImagesCreateParameters { FromImage = _image },
+                authConfig: null,
+                new Progress<JSONMessage>()
+            );
+
+            return;
+        }
+
+        var images = await _client.Images.ListImagesAsync(
+            new ImagesListParameters
+            {
+                Filters = new Dictionary<string, IDictionary<string, bool>>()
+                {
+                    ["reference"] = new Dictionary<string, bool> { [_image] = true },
+                },
+            }
+        );
+
+        if (images.Count == 0)
+            throw new InvalidOperationException(
+                $"Local runner image '{_image}' not found. Run at project root: docker build -t {_image} ./src/Runner"
+            );
+    }
 }
